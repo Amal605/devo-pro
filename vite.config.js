a5d2cfa84facd559f84f14a2b84a0e4b60ff9a12
Amal605 <amaltarek631@gmail.com>
2024-12-07 11:29:33 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/DevFolio-Project/",
   plugins: [react()],
+  base: "/devFolio-project",
 })
